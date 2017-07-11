0160ed081d80a4484d01965098a8f2af4c6cfd23
root <root@4b91596cba88>
2017-07-11 11:37:06 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "vermillion",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "WMTheme",
+  "homePage" : "Main",
+  "name" : "WMTheme",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en,es-es,fr,hi,te-in,pt,ar-iq",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
